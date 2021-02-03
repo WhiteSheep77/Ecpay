8ae9d61c887751d9f79bf27a77f8eb453293e32a
WhiteSheep77 <peterpeteraa7803@gmail.com>
2021-02-03 11:42:32 +0800

    1

---
+++ Ecpay.go
@@ -0,0 +1,53 @@
+package Ecpayby77
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func generateMerchantTradeNo() (No string) {
+	timeNow := time.Now()
+	timeString := timeNow.Format("20060102150405000")
+	No = timeString
+	Count := strings.Count(No, "")
+
+	fmt.Print("string Count=", Count, " MerchantTradeNo=", No, "\n")
+	return No
+}
+
+func FormUrlEncode(s string) string {
+	s = url.QueryEscape(s)
+	//s = strings.ReplaceAll(s, "%2d", "-")
+	//s = strings.ReplaceAll(s, "%5f", "_")
+	//s = strings.ReplaceAll(s, "%2e", ".")
+	s = strings.ReplaceAll(s, "%21", "!")
+	s = strings.ReplaceAll(s, "%2A", "*")
+	s = strings.ReplaceAll(s, "%28", "(")
+	s = strings.ReplaceAll(s, "%29", ")")
+	return s
+}
+
+/*
+綠界的參數請參考 https://www.ecpay.com.tw/Content/files/ecpay_011.pdf
+
+MemberId 自己商場會員的ID 用於自己識別
+MerchantID  綠界參數
+TotalAmount 綠界參數
+*/
+
+func SendPostToEcPay(MemberId int, MerchantID string, TotalAmount int, TradeDesc string, ItemName string) (err error) {
+	MerchantTradeNo := generateMerchantTradeNo()
+	MerchantTradeDate := time.Now().Format("2006/01/02 15:04:05")
+	PaymentType := "aio"
+	TradeDesc = FormUrlEncode(TradeDesc)
+
+	fmt.Print("\nMerchantTradeNo=", MerchantTradeNo)
+	fmt.Print("\nMerchantTradeDate=", MerchantTradeDate)
+	fmt.Print("\nPaymentType=", PaymentType)
+	fmt.Print("\nTradeDesc=", TradeDesc)
+	fmt.Print("\nItemName=", ItemName)
+	fmt.Print("\n")
+	return
+}
---
+++ test/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/WhiteSheep77/Ecpay"
+)
+
+func main() {
+	Ecpayby77.SendPostToEcPay(12, "2000132", 2399, "85VIP", "VIP30天")
+}
